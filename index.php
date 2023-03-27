4bef2aad3e397167c35cbb9b7c29415bec4b4373
Pliv <y.dekker02@student.alfa-college.nl>
2023-03-27 13:32:32 +0200

    uhhh yeah!

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+$activePage = "/site";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="icon" href="img/favicon.ico">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>website</title>
+    <link rel="stylesheet" href="/css/main.css"
+    
+</head>
+
+<body class="body">
+
+
+
+<?php require 'nav.php'?>
+</body>
+<script src="js/main.js"></script>
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</html>
---
+++ nav.php
@@ -0,0 +1,28 @@
+<?php
+$pages = array();
+$pages["/portofolio"] = "Portofolio";
+$pages["/site"] = "Home";
+$pages["/projecten"] = "Projecten";
+
+$icons = ["person-sharp", "home-sharp", "code-sharp"];
+$counter = 0;
+?>
+
+<link rel="stylesheet" href="/css/nav.css">
+<div class="wrapper">
+    <div class="nav" id="center">
+        <ul>
+            <?php foreach ($pages as $url => $title): ?>
+                <li <?php if ($url === $activePage): ?>class="list active"<?php else: ?> class="list"<?php endif ?>>
+                    <a href="<?php echo $url ?>">
+                        <div class="indicator"></div>
+                        <span class="icon"><ion-icon name="<?php echo $icons[$counter]; ?>"></ion-icon></span>
+                        <span class="text"><?php echo $title; ?></span>
+                    </a>
+                </li>
+                <?php $counter++ ?>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+</div>
+
