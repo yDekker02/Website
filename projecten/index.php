80e89d0478dfe1158393a397507b2ea210df4961
Pliv <y.dekker02@student.alfa-college.nl>
2023-06-02 12:24:02 +0200

    esrgd

--- projecten/index.php
+++ projecten/index.php
@@ -2,22 +2,25 @@
 $activePage = "/projecten";
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
+<html class="hydrated" lang="en"><head>
+    <style data-styles="">ion-icon{visibility:hidden}.hydrated{visibility:inherit}</style>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>website</title>
     <link rel="stylesheet" href="/css/main.css"
-    
 </head>
 
 <body class="body">
-
-
-
 <?php require '../nav.php'?>
+
+<div class="link-wrapper">
+    <a href = "/onepager" target="new" class = "link"><button>onepager/irritante website</button></a>
+    <a href = "https://github.com/yDekker02" target="new" class = "link"><button>github</button></a>
+    <a href = "https://github.com/Plivbot/Committee-Public" target="new" class = "link"><button>minecraft hack client (java)</button></a>
+    <a href = "" class = "link"><button>balls</button></a>
+    <a href = "" class = "link"><button>balls</button></a>
+</div>
 </body>
 <script src="/js/main.js"></script>
 </html>
