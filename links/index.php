45035987abe93e38fb3fe406cc67c5687f5d8d5e
Pliv <y.dekker02@student.alfa-college.nl>
2023-07-04 16:02:17 +0200

    added wip page

--- links/index.php
+++ links/index.php
@@ -9,14 +9,19 @@ $activePage = "/links";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>website</title>
-    <link rel="stylesheet" href="/css/portofolio.css"
-    
+    <link rel="stylesheet" href="/css/links.css"
+
 </head>
 
 <body class="body">
 
+<div class="wrapper">
+    <img src="/img/monkey.png">
+    <h1>Page is W.I.P.</h1>
+</div>
 
-<?php require '../nav.php'?>
+
+<?php require '../nav.php' ?>
 </body>
 <script src="/js/main.js"></script>
 </html>
